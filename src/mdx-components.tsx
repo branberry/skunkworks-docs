d068b0a25dd0a7e8c1517042f5dde7449529ae9f
Caesar Bell <caesar.bell@mongodb.com>
2024-05-15 12:35:30 -0400

    :sparkles: Adds global component and some simple mock for a page

--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -1,7 +1,11 @@
 import type { MDXComponents } from "mdx/types";
+import { Body, H1, H2, H3, Subtitle } from "@/components/typography";
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
+    h1: ({ children }) => (
+      <H2 style={{ color: 'black'}}> {children} </H2>
+    ),
     ...components,
   };
 }
