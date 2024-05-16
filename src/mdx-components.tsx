1ff8b06cc582fdcd83cb1f35e79769a97ec39912
Caesar Bell <caesar.bell@mongodb.com>
2024-05-16 16:23:26 -0400

    :bug: Removes FOUC

--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -1,10 +1,11 @@
 import type { MDXComponents } from "mdx/types";
-import { Body, H1, H2, H3, Subtitle } from "@/components/typography";
+import { H2 } from "@/components/typography";
+
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
     h1: ({ children }) => (
-      <H2 style={{ color: 'black'}}> {children} </H2>
+      <H2 style={{ color: '#000' }}> {children} </H2>
     ),
     ...components,
   };
